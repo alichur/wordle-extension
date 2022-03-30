046ed3c14a98127aac0e87e47e6bb3e1e0a42e71
Ali Churcher <churcher.ali@gmail.com>
2022-03-31 12:48:34 +1300

    remove test code

--- background.js
+++ background.js
@@ -1,13 +1,7 @@
 let clipboardText = 'RESULTS: my wordle results are';
 
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.storage.sync.set({ clipboardText });
-//     console.log(`Clipboard text will be set to: ${clipboardText}`);
-// });
-
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.storage.sync.set({ clipboardText });
+    console.log(`Clipboard text will be set to: ${clipboardText}`);
 });
+
--- popup.js
+++ popup.js
@@ -1,28 +1,22 @@
-// Initialize butotn with users's prefered color
-let copyToClipboard = document.getElementById("copyToClipboard");
+let copyButton = document.getElementById("copyButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  copyToClipboard.style.backgroundColor = color;
-});
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-copyToClipboard.addEventListener("click", async () => {
+// When the button is clicked, inject copyToClipboard into current page
+copyButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: copyToClipboard,
   });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
+function copyToClipboard() {
   alert('test')
   chrome.storage.sync.get("clipboardText", ({ clipboardText }) => {
     alert(`clipboard text is ${clipboardText}`);
   });
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+
 }
