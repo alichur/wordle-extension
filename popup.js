fe98637b6288fd17b0e0734709c6f15c5ecf1ac0
Ali <churcher.ali@gmail.com>
2022-03-31 16:30:32 +1300

    Copies quordle results to clipbloard (#2)
    Allows quordle page to be opened and results copied to clipboard

--- popup.js
+++ popup.js
@@ -1,24 +1,31 @@
-// Initialize butotn with users's prefered color
-let getResults = document.getElementById("getResults");
+let copyButton = document.getElementById("copyButton");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  getResults.style.backgroundColor = color;
-});
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-getResults.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// When the button is clicked, inject copyToClipboard into current page
+copyButton.addEventListener("click", async () => {
+  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  // let wordle = await chrome.tabs.create({ active: false, url: "https://www.nytimes.com/games/wordle/index.html" });
+  let quordle = await chrome.tabs.create({ active: false, url: "https://www.quordle.com/#/" });
+  alert("test")
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    target: { tabId: quordle.id },
+    function: copyToClipboard,
   });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function copyToClipboard() {
+  alert('testing');
+
+  chrome.storage.sync.get("clipboardText", ({ clipboardText }) => {
+    alert(`clipboard text updated is ${clipboardText}`);
+    let quordleInnerButtonDiv = [...document.querySelectorAll('div')].filter(el => el.innerHTML === 'Copy to Clipboard');
+    let quordleCopyButton = quordleInnerButtonDiv[0].closest("button");
+    quordleCopyButton.click();
+    alert("clicked quordle test");
+
   });
+
 }
