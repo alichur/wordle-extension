0c71f4e4d6c22d7815c2ccbc108f66ce265e1c3d
Ali Churcher <churcher.ali@gmail.com>
2022-03-31 13:07:19 +1300

    opens tabs for wordle and quordle

--- popup.js
+++ popup.js
@@ -4,7 +4,8 @@ let copyButton = document.getElementById("copyButton");
 // When the button is clicked, inject copyToClipboard into current page
 copyButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+  let wordle = await chrome.tabs.create({ active: false, url: "https://www.nytimes.com/games/wordle/index.html" });
+  let quordle = await chrome.tabs.create({ active: false, url: "https://www.quordle.com/#/" });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: copyToClipboard,
