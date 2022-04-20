1eecad2405ab49afd3af5422e8e127919d3bbe41
Ali <churcher.ali@gmail.com>
2022-04-20 14:10:55 +1200

    Append to clipboard (#3)
    Format octordle and qurodle results and copy to user clipboard

--- background.js
+++ background.js
@@ -1,7 +1,27 @@
-let clipboardText = 'RESULTS: my wordle results are';
+let initialResults = 'game results';
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ clipboardText });
-    console.log(`Clipboard text will be set to: ${clipboardText}`);
+    let results = `${initialResults} are`;
+    chrome.storage.sync.set({
+        quordle: results,
+        durdle: results,
+        octordle: results,
+        formatted: results
+    }, function () {
+        console.log(`4 vars set`);
+    });
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+    // console.log(`${Object.keys(changes)[0]} storage value updated`)
+    if ("quordle" in changes) {
+        console.log(changes.quordle.newValue);
+    };
+    if ("octordle" in changes) {
+        console.log(changes.octordle.newValue);
+    };
+    if ("durdle" in changes) {
+        console.log(changes.durdle.newValue);
+    };
 });
 
--- popup.js
+++ popup.js
@@ -1,31 +1,119 @@
+// For each game, open a new tab and click the share button.
+// Paste from the clipboard into a temporary element, then save the value from this element into chrome storage.
+// Finally, results from storage are formatted into a single string, added to another temporary element, then copied into the user's clipboard.
+
 let copyButton = document.getElementById("copyButton");
 
 
-// When the button is clicked, inject copyToClipboard into current page
+
 copyButton.addEventListener("click", async () => {
-  // let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  // let wordle = await chrome.tabs.create({ active: false, url: "https://www.nytimes.com/games/wordle/index.html" });
+  let wordle = "https://www.nytimes.com/games/wordle/index.html";
   let quordle = await chrome.tabs.create({ active: false, url: "https://www.quordle.com/#/" });
-  alert("test")
+  let durdle = await chrome.tabs.create({ active: false, url: "https://zaratustra.itch.io/dordle" });
+  let octordle = await chrome.tabs.create({ active: false, url: "https://octordle.com/?mode=daily" });
+
+  // wait for page to load. Can't look for share button we can't tell between dialog not loading due to time, or due to puzzle being incomplete.
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
   chrome.scripting.executeScript({
     target: { tabId: quordle.id },
-    function: copyToClipboard,
+    function: handleQurdle,
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: durdle.id },
+    function: handleDurdle,
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: octordle.id },
+    function: handleOctordle,
+  });
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  chrome.storage.sync.get(['quordle', 'octordle'], function (resultArray) {
+    let summary = resultArray['octordle'] + resultArray['quordle'];
+    let resultsTextArera = document.createElement("textarea");
+    resultsTextArera.setAttribute("id", "results");
+    document.body.appendChild(resultsTextArera);
+    resultsTextArera.value = summary;
+    resultsTextArera.focus();
+    resultsTextArera.select();
+    document.execCommand('copy');
+
+    chrome.tabs.remove([octordle.id, quordle.id, durdle.id]);
+    window.close();
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function copyToClipboard() {
-  alert('testing');
+async function handleQurdle() {
+  let qurodle = "https://www.quordle.com/#/";
 
-  chrome.storage.sync.get("clipboardText", ({ clipboardText }) => {
-    alert(`clipboard text updated is ${clipboardText}`);
-    let quordleInnerButtonDiv = [...document.querySelectorAll('div')].filter(el => el.innerHTML === 'Copy to Clipboard');
-    let quordleCopyButton = quordleInnerButtonDiv[0].closest("button");
-    quordleCopyButton.click();
-    alert("clicked quordle test");
+  function getFormattedQuordle(fullQuordleString) {
+    return fullQuordleString.includes('quordle.com') ? fullQuordleString.split('quordle.com')[0] : qurodle;
+  };
 
+  //copy quordle results to clipboard
+  let quordleInnerButtonDiv = [...document.querySelectorAll('div')].filter(el => el.innerHTML === 'Copy to Clipboard');
+  let quordleCopyButton = quordleInnerButtonDiv[0].closest("button");
+  quordleCopyButton.click();
+
+  // paste from clipboard and copy into storage
+  let textArea = document.createElement("textarea");
+  textArea.setAttribute("id", "pasteArea");
+  document.body.appendChild(textArea);
+  textArea.focus();
+  document.execCommand('paste');
+  text = document.getElementById("pasteArea").value;
+  let updatedText = getFormattedQuordle(text);
+  chrome.storage.sync.set({ quordle: updatedText }, function () {
+    textArea.remove()
   });
+}
 
+async function handleDurdle() {
+  let durdle = "https://zaratustra.itch.io/dordle";
+  function getFormattedDurdle(fullDurdleString) {
+    return fullDurdleString.includes('7') ? fullDurdleString.split('7')[0] : durdle;
+  };
+
+  // document.getElementById("game_drop").contentWindow.document
+  //copy durdle results to clipboard
+  let button = document.getElementById("share");
+  // document.getElementById("game_drop")
+  button.click();
+
+  // paste from clipboard and copy into storage
+  let textArea = document.createElement("textarea");
+  textArea.setAttribute("id", "pasteArea2");
+  document.body.appendChild(textArea);
+  textArea.focus();
+  document.execCommand('paste');
+  text = document.getElementById("pasteArea2").value;
+  text = getFormattedDurdle(text);
+  chrome.storage.sync.set({ durdle: text }, function () {
+    textArea.remove()
+  });
+}
+
+async function handleOctordle() {
+  let octordle = "https://octordle.com/?mode=daily";
+  function getFormattedOctordle(fullOctordleString) {
+    return fullOctordleString.includes('octordle.com') ? fullOctordleString.split('octordle.com')[0] : octordle;
+  };
+
+  document.getElementById("share_clipboard").click();
+
+  // paste from clipboard and copy into storage
+  let textArea = document.createElement("textarea");
+  textArea.setAttribute("id", "pasteArea3");
+  document.body.appendChild(textArea);
+  textArea.focus();
+  document.execCommand('paste');
+  text = document.getElementById("pasteArea3").value;
+  text = getFormattedOctordle(text);
+  chrome.storage.sync.set({ octordle: text }, function () {
+    textArea.remove()
+  });
 }
