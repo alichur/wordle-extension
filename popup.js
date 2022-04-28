6eee7145a80697ae1adae1a872f06019ab9430ae
Ali Churcher <churcher.ali@gmail.com>
2022-04-28 16:44:49 +1200

    small change

--- popup.js
+++ popup.js
@@ -122,7 +122,7 @@ async function handleOctordle() {
     document.execCommand('paste');
     text = document.getElementById("octordleResult").value;
     text = text.split('octordle.com')[0];
-    if (text.length < 1) {
+    if (text[0].length < 1) {
       throw "Puzzle results not available, is the puzzle compeleted?";
     }
     chrome.storage.sync.set({ octordle: text }, function () {
